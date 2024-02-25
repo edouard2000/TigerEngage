d0c712e7a6b64f1324636135ae677d2e2ebd6f8a
Edouard <ek4149@princeton.edu>
2024-02-24 22:39:57 -0500

    added a components to display class list

---
+++ src/components/Class/DisplayClass.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'; 
+
+const DisplayList = ({ classes }) => {
+  return (
+    <ul className="space-y-2">
+      {classes.map((className, index) => (
+        <li
+          key={index}
+          className="cursor-pointer rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-700 focus:outline-none"
+          tabIndex={0}
+        >
+          {className}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+DisplayList.propTypes = {
+  classes: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default DisplayList;
