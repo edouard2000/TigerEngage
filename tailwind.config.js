5df2722b773bc313e8c3a9a812826684de551875
Edouard <ek4149@princeton.edu>
2024-03-20 00:40:50 -0400

    Finalised the home pahe and login page. And added their routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  theme: {
+    extend: {
+      height: {
+        header: "80px",
+        footer: "40px",
+      },
+    },
+  },
   content: ["./templates/**/*.html", "./static/js/**/*.js"],
 };
