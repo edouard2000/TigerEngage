6f2539fe96fb0ac3168c647fd89d29dfa9487a4f
Edouard <ek4149@princeton.edu>
2024-03-23 23:47:11 -0400

    completed the student dashboard and completly responsive to all devices

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+classes = [
+    {
+        "name": "Introduction to Python",
+        "instructor": "Prof. John Doe",
+        "is_active": True,
+    },
+    {
+        "name": "Advanced Mathematics",
+        "instructor": "Dr. Jane Smith",
+        "is_active": False,
+    },
+]
+
 
 @app.route("/")
 def home():
@@ -16,6 +29,13 @@ def login():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+
+@app.route('/student_dashboard')
+def student_dashboard():
+    student_name = "John Doe" 
+    return render_template('student-dashboard.html', student_name=student_name, classes=classes)
+
 
 
 if __name__ == "__main__":
