ffd52a0724f8e6878f94d43a9a41a5d020cd6b33
Roshaan Khalid <roshaan@princeton.edu>
2024-04-19 16:09:47 -0400

    notes

--- static/main.js
+++ static/main.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function() {
     submit_btn.addEventListener('click', function() {
         const transcript = transcriptArea.value;
         if (transcript) {
-            fetch('https://tigerengage.com/process_transcript', {
+            fetch('/process_transcript', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
