e94868dd830eb6b81b60cf1fe78e3cec03ca7bf0
Edouard <ek4149@princeton.edu>
2024-04-29 13:24:16 -0400

    added a page and route to handle the 404 erorr, like accessing wrong url

--- app.py
+++ app.py
@@ -1038,5 +1038,10 @@ def display_answers_for_question(class_id):
             os.abort(404, description="Resource not found")
     return render_template('answers-page.html', question_text=question_text, answers=answers)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     socketio.run(app)
